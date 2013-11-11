9b3fd65be1a78ec0f08d8df2db7343abf524de3f
elviejo79 <agarciafdz@gmail.com>
2013-11-10 23:50:00 -0600

    refs #1: add golint to the list of things to run before commiting

--- _scripts/hooks/pre-commit.sh
+++ _scripts/hooks/pre-commit.sh
@@ -17,10 +17,11 @@ unformatted=$(gofmt -l $gofiles)
 [ -z "$unformatted" ] && exit 0
 
 # Some files are not gofmt'd. Print message and fail.
-
 echo >&2 "Go files must be formatted with gofmt. Please run:"
 for fn in $unformatted; do
     echo >&2 "  gofmt -w $PWD/$fn"
 done
 
+# a precaution
+echo "Check your files style with _scripts/hook/golint <filename> and check the suggestion"
 exit 1
