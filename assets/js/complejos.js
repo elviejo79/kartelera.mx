843c49eb4c12da980e2924d77ea1ba0ae2611fc3
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2013-11-23 14:39:11 -0600

    Muestra los complejos donde se muestra una película

--- assets/js/complejos.js
+++ assets/js/complejos.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-	$("#horario1").click(function(event){
-		$("#complejos1").toggle();
+		$( ".date" ).each(function() {
+		$(this).click(function(){
+			var number = $(this).attr("id").slice(7);
+
+			var item = $("#complejos" + number);
+			$(".complejos:visible").toggle();
+			item.toggle();
+		});
 	});
+
 });
