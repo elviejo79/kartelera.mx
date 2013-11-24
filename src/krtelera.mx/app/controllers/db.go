3baf1d4d84b5b58ab537c326f95f3fad310511b4
poz2k4444 <poz2k4444@gmail.com>
2013-11-24 17:49:10 -0600

    van con mejor estilo

--- src/krtelera.mx/app/controllers/db.go
+++ src/krtelera.mx/app/controllers/db.go
@@ -8,7 +8,7 @@ import (
 //        "os"
 )
 
-const dataBase string = "/home/ubuntu/kartelera.mx/scrapping/foo.db"
+const dataBase string = "/home/user/Documents/code/github/kartelera.mx/scrapping/foo.db"
 
 type Film struct {
      Cine 	 string	
